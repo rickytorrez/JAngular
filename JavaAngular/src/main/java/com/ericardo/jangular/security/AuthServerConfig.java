686fced1772b1c4c1b273ee8a6508d88d637480c
rickytorrez <torrez.ricky@ymail.com>
2018-07-01 23:49:53 -0400

    OAuth

---
+++ JavaAngular/src/main/java/com/ericardo/jangular/security/AuthServerConfig.java
@@ -0,0 +1,33 @@
+package com.ericardo.jangular.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	AuthenticationManager _aM;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().
+			withClient("client").
+			secret("{noop}secret").
+			authorizedGrantTypes("password").
+			resourceIds("oauth2-resource").
+			scopes("read");
+	}
+	
+	@Override
+	// Share authentication manager with WebSecurity
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(_aM);
+	}
+}
--- JavaAngular/src/main/java/com/ericardo/jangular/security/WebSecurity.java
+++ JavaAngular/src/main/java/com/ericardo/jangular/security/WebSecurity.java
@@ -1,5 +1,7 @@
 package com.ericardo.jangular.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -7,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("bob").password("{noop}123").roles("USER");
